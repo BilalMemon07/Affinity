e8db6e7062c81c9f968993c371a26b15c427e270
BilalMemon07 <bilalsaylani451@gmail.com>
2022-12-26 11:59:25 +0500

    Updated Odoo API

--- odoo_API/odoo_api.py
+++ odoo_API/odoo_api.py
@@ -56,11 +56,11 @@ def getUsers():
     return "User Login Successfully"
 
 
-# @app.route('/checkingData', methods=['POST'])
-# def checkingData():
-#     return data
-tokenList = []
 
+tokenList = []
+@app.route('/', methods=['GET'])
+def get():
+    return "Hello"
 
 @app.route(url_prefix + 'login', methods=['POST'])
 def login():
@@ -91,95 +91,43 @@ def login():
 @app.route(url_prefix + 'createCustomer', methods=['POST'])
 def createSaleOrder():
     record = json.loads(request.data)
-    # check_payload = [
-    #     'customerName',
-    #     'gender',
-    #     'mobile',
-    #     'cnicNumber',
-    #     'fatherName',
-    #     'cnicExpiryDate',
-    #     'dateOfBirth',
-    #     'birthPlace',
-    #     'mailingAddress',
-    #     'permanentAddress',
-    #     'city',
-    #     'province',
-    #     'natureOfBusiness',
-    #     'requestorIncome',
-    #     'noOfYears',
-    #     'companyName',
-    #     'companyAddress',
-    #     'cnicFront',
-    #     'cnicBack',
-    #     'securityCheque',
-    #     'customerImage',
-    # ]
     try:
         global authToken
         res = authentication(authToken)
         if res == True:
+            
             id = models.execute_kw(login_data['db'], uid, login_data['dbpassword'], 'res.partner', 'create', [{
-                'customer_name': record['customer_name'],
-                'gender':record['gender'],
-                'mobile':record['mobile'],
-                'cnic_number':record['cnic_number'],
-                'father_name':record['father_name'],
-                'cnic_expiry_date':record['cnic_expiry_date'],
-                'date_of_birth':record['date_of_birth'],
-                'birth_place':record['birth_place'],
-                'mailing_address':record['mailing_address'],
-                'permanent_address':record['permanent_address'],
-                'city':record['city'],
-                'province':record['province'],
-                'nature_of_business':record['nature_of_business'],
-                'requestor_income':record['requestor_income'],
-                'no_of_years':record['no_of_years'],
-                'company_name':record['company_name'],
-                'company_address':record['company_address'],
-                'cnic_front':record['cnic_front'],
-                'cnic_back':record['cnic_back'],
-                'security_cheque':record['security_cheque'],
-                'customer_image':record['customer_image'],
+                "customer_name":record['customer_name'],
+                "name":record['customer_name'],
+                "gender":record['gender'],
+                "cnic_number":record['cnic_number'],
+                "father_name":record['father_name'],
+                "cnic_expiry_date":record['cnic_expiry_date'],
+                "date_of_birth":record['date_of_birth'],
+                "birth_place":record['birth_place'],
+                "mailing_address":record['mailing_address'],
+                "permanent_address":record['permanent_address'],
+                "city":record['city'],
+                "province":record['province'],
+                "nature_of_business":record['nature_of_business'],
+                "requestor_income":record['requestor_income'],
+                "no_of_years":record['no_of_years'],
+                "company_name":record['company_name'],
+                "company_address":record['company_address'],
+                "cnic_front":record['cnic_front'],
+                "cnic_back":record['cnic_back'],
+                "security_cheque":record['security_cheque'],
+                "customer_image":record['customer_image'],
             }])
-            # return 'Record created successfully! '+'Record Created ID is '+  id
+            print(id)
             return Response('Record created successfully! ', status=201, mimetype='application/json')
         else:
             return Response(res, status=400, mimetype='application/json')
 
-    except exception as ex:
+    except NameError as ex:
         print(ex)
-        return Response('Sorry, something went wrong!' + str(NameError), status=400, mimetype='application/json')
+        return Response('Sorry, something went wrong!' + str(ex), status=400, mimetype='application/json')
 
-
-# @app.route('/getQuotationByName/<orderNumber>', methods=['GET'])
-# def getQuotationByName(orderNumber):
-#     try:
-#         orderNum = orderNumber
-#         global authToken
-#         res = authentication(authToken)
-#         if not orderNum:
-#             return Response('Record Number Must be Required in Url ', status=404, mimetype='application/json')
-#         if res == True:
-#             order_line_data_list = []
-#             data = models.execute_kw(login_data['db'], uid, login_data['dbpassword'], 'sale.order', 'search_read', [
-#                                      [['name', '=', orderNum]]])
-#             sale_order_line = models.execute_kw(
-#                 login_data['db'], uid, login_data['dbpassword'], 'sale.order.line', 'search_read', [[]], {'fields': ['name', 'order_id', 'product_id', 'product_uom_qty', 'product_uom', 'price_unit', 'tax_id']})
-#             for rec in data:
-#                 for line in sale_order_line:
-#                     if rec['id'] == line['order_id'][0]:
-#                         order_line_data_list.append(line)
-#                         rec['order_line_data'] = order_line_data_list
-#                         # new key, so add
-#                 # print(rec)
-#             print(order_line_data_list)
-#             return jsonify(data)
-#         else:
-#             print("hello False")
-#             return Response(res, status=400, mimetype='application/json')
-#     except Exception as ex:
-#         print('Sorry, something went wrong!')
-#         return Response('Sorry, something went wrong!' + ' ' + str(ex), status=400, mimetype='application/json')
 
 
 @app.route(url_prefix + 'updateCustomer/<id>', methods=['POST'])
@@ -190,11 +138,11 @@ def updatesalesorder(id):
         global authToken
         res = authentication(authToken)
         if res == True:
-            orderId = models.execute_kw(login_data['db'], uid, login_data['dbpassword'], 'sale.order', 'search', [
+            orderId = models.execute_kw(login_data['db'], uid, login_data['dbpassword'], 'res.partner', 'search', [
                 [['id', '=', customer_id]]])
             if not orderId:
                 return Response('Please Enter Correct Order Number!', status=400, mimetype='application/json')
-            data = models.execute_kw(login_data['db'], uid, login_data['dbpassword'], 'sale.order', 'write', [[customer_id],record])
+            data = models.execute_kw(login_data['db'], uid, login_data['dbpassword'], 'res.partner', 'write', [[customer_id],record])
             if not data:
                 return Response('Please Enter Correct Order Number!', status=400, mimetype='application/json')
             return Response('Record Update successfully!', status=201, mimetype='application/json')
@@ -211,3 +159,27 @@ if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
     # from waitress import serve
     # serve(app,host='127.0.0.1',port=5000)
+
+
+
+
+# "gender":"male",
+# "mobile":false,
+# "cnic_number":"e1231eqweqwe",
+# "father_name":"AKkkss",
+# "cnic_expiry_date":false,
+# "date_of_birth":false,
+# "birth_place":"karachi",
+# "mailing_address":"hello",
+# "permanent_address":"dadasd",
+# "city":"adadad",
+# "province":"dasdasd",
+# "nature_of_business":"dadasdasd",
+# "requestor_income":false,
+# "no_of_years":false,
+# "company_name":"asd",
+# "company_address":"dasda",
+# "cnic_front":false,
+# "cnic_back":false,
+# "security_cheque":false,
+# "customer_image":false
