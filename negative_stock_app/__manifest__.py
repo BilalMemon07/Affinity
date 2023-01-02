cad2c9295d09dcba8a47e0318bfa6d66db3ffa0a
BilalMemon07 <bilalsaylani451@gmail.com>
2023-01-02 19:27:57 +0500

    Negative App

---
+++ negative_stock_app/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": " Negative Stock",
+    "version": "15.1",
+    "category": "Inventory, Logistic, Storage",
+    "license": "AGPL-3",
+    "summary": "Disallow negative stock levels by default",
+    "author": "Muhammad Bilal",
+    "website": "",
+    "depends": ["stock"],
+    "data": [
+        "views/product.xml",
+        "views/stock_location.xml"
+        ],
+    "installable": True,
+}
