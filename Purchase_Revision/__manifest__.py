c4c1c0ca33b3663118a19ffb51361fbfa4aaf96b
BilalMemon07 <bilalsaylani451@gmail.com>
2023-01-03 12:19:50 +0500

    Add Purchase Revision App

---
+++ Purchase_Revision/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Purchase Revision','sequence': 0,
+    'category': 'Purchase',
+    'version': '0.1',
+    'summary': 'Purchasse Revision History',
+    'description': """Purchasse Revision History""",
+    'depends': ['purchase','purchase_stock'],
+    'author': "Muhammad Bilal Memon",
+    'data': [
+        'views/purchase_views.xml'
+        ],
+    'auto_install': True,
+    'images' : [],
+    'assets': {},
+    'license': 'LGPL-3',
+}
